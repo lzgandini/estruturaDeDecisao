f835ff98fb8022a2bcf69642dabd26874d36b61d
Luiza Gandini <59964341+lzgandini@users.noreply.github.com>
2022-04-06 19:35:15 -0300

    Create #10.py

---
+++ #10.py
@@ -0,0 +1,15 @@
+"""
+#10 Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou V-Vespertino ou N- Noturno.
+ Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
+"""
+
+turno = (input("Digite a letra inicial do turno em que você estuda\n(M-Matutino ou V-Vespertino ou N-Noturno): ")).upper()
+
+if turno == "M":
+    print("Bom dia!")
+elif turno == "V":
+    print("Boa tarde!")
+elif turno == "N":
+    print("Boa noite!")
+else:
+    print("Valor inválido.")
